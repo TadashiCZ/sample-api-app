94671b4ff2c3242f75d74d601db4fb6c00369cbd
TadashiCZ <tadashicz@gmail.com>
2020-04-07 17:06:16 +0200

    add basic detail without image

--- SampleApiApp/app/src/main/java/cz/tadeasvalenta/sample/ui/DetailActivity.kt
+++ SampleApiApp/app/src/main/java/cz/tadeasvalenta/sample/ui/DetailActivity.kt
@@ -6,16 +6,24 @@ import android.widget.Toast
 import cz.tadeasvalenta.sample.R
 import cz.tadeasvalenta.sample.constants.NetworkConstants
 import cz.tadeasvalenta.sample.domain.responses.RepoDataItem
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        val repo : RepoDataItem? = intent.extras?.getParcelable(NetworkConstants.REPOSITORY_BUNDLE)
-        if (repo!= null){
-            Toast.makeText(applicationContext, repo.name, Toast.LENGTH_LONG).show()
+        val repo: RepoDataItem? = intent.extras?.getParcelable(NetworkConstants.REPOSITORY_BUNDLE)
+        if (repo != null) {
+            tvRepoName.text = repo.name
+            tvAuthorLogin.text = getString(R.string.author_login, repo.owner.login)
+            tvCreatedAt.text = getString(R.string.created_at, repo.createdAt)
+            tvFork.text = getString(R.string.fork, if (repo.fork) "yes" else "no")
+            tvStars.text = getString(R.string.stars, repo.stargazersCount.toString())
+            tvScore.text = getString(R.string.score, repo.score.toString())
+            tvDescription.text = getString(R.string.description, repo.description)
         } else {
             Toast.makeText(applicationContext, "NO DATA", Toast.LENGTH_LONG).show()
         }
